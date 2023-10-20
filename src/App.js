f2be605a501b0eef4fcd32d4808361965fb42a8e
Shaunpaul Anthony <98692165+ShaunpaulAnthony@users.noreply.github.com>
2023-10-20 07:39:17 -0400

    initial react setup and header

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from './components/Header'
+
+function App() {
+  return (
+    <BrowserRouter>
+        <div className="App">
+            <Header />
+        </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
