5a8823817a8d0fb4c2fcb78c356209a30c1b1ecc
Shaunpaul Anthony <98692165+ShaunpaulAnthony@users.noreply.github.com>
2023-10-27 02:28:10 -0400

    portfolio update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Testimonials from "./components/Testimonials";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from './components/Header'
+import './App.css';
+
 
 function App() {
   return (
     <BrowserRouter>
-        <div className="App">
-            <Header />
-        </div>
+        <main className="text-gray-400 bg-gray-900 body-font">
+            <Navbar />
+            <About />
+            <Skills />
+            <Projects />
+        </main>
     </BrowserRouter>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,35 @@
+export const projects = [
+    {
+      title: "5 Star Cinema",
+      subtitle: "MySQL, Express, React, NodeJS",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-1.gif",
+      link: "https://reactbootcamp.com",
+    },
+    {
+      title: "React Tracks",
+      subtitle: "React and Python",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-2.gif",
+      link: "https://reedbarger.com",
+    },
+  ];
+
+  export const skills = [
+    "JavaScript",
+    "React",
+    "Node.js",
+    // ... other skills
+  ];
+
+  export const testimonials = [
+    {
+      name: "Jane Doe",
+      company: "ABC Corp",
+      quote: "The project was delivered on time and exceeded our expectations.",
+      image: "./path_to_image.jpg"
+    },
+    // ... other testimonials
+  ];
